1d3c715afbc0cc6a170bdc094d7af17fb25552a3
pmarmor <politotuto@gmail.com>
2022-11-17 18:54:11 +0100

    Creación de la clase DVD

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <?php
 include "Soporte.php";
-
-$soporte1 = new Soporte("Tenet", 22, 3);
-echo "<strong>" . $soporte1->titulo . "</strong>";
-echo "<br>Precio: " . $soporte1->getPrecio() . " euros";
-echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros";
-$soporte1->muestraResumen();
+include "Dvd.php";
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+echo "<strong>" . $miDvd->titulo . "</strong>";
+echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
+echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
+$miDvd->muestraResumen();
