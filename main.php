5148972d308c805449b3118d1ee8789ad5708232
pmarmor <politotuto@gmail.com>
2022-11-17 19:11:43 +0100

    EJERCICIO4: creación de la clase Juego

--- main.php
+++ main.php
@@ -1,8 +1,9 @@
 <?php
 include "Soporte.php";
-include "Dvd.php";
-$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
-echo "<strong>" . $miDvd->titulo . "</strong>";
-echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
-echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
-$miDvd->muestraResumen();
+include "Juego.php";
+
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 2);
+echo "<strong>" . $miJuego->titulo . "</strong>";
+echo "<br>Precio: " . $miJuego->getPrecio() . " euros";
+echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+$miJuego->muestraResumen();
